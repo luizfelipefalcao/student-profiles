cde65863f6e7bb14b0b3bf0536ccab8cac5f6b2e
Luiz <falcao.lfj@yahoo.com>
2021-07-14 00:55:39 -0700

    first layout done

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,35 +1,68 @@
 import React, { useEffect, useState } from 'react';
+import './home.css';
 
 const Home = () => {
     const [profiles, setProfiles] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [searchName, setSearchName] = useState('');
+    const [searchTag, setSearchTag] = useState('');
 
     const fetchData = async () => {
         await fetch(`https://api.hatchways.io/assessment/students`)
             .then((res) => res.json())
-            .then(({students}) => setProfiles(students))
+            .then(({ students }) => setProfiles(students))
             .then(setLoading(false));
     };
 
     useEffect(() => {
         setLoading(true);
         fetchData();
-        console.log(profiles);
-    }, [profiles]);
+    }, []);
 
     return (
-        <div>
-            {profiles.map((list) => (
-                <ul key={list.id}>
-                    <li><img src={list.pic}/></li>
-                    <li>Fullname: {' '} {list.firstName} {list.lastName}</li>
-                    <li>City: {' '} {list.city}</li>
-                    <li>Company: {' '} {list.company}</li>
-                    <li>Email: {' '} {list.email}</li>
-                    <li>Grades {' '} {list.grades.map(item => <p>{item}</p>)}</li>
-                </ul>
-            ))}
-        </div>
+        <>
+            {loading ? (
+                <h2 style={{ fontWeight: 'bold' }}>Loading ...</h2>
+            ) : (
+                <div className='profile-list'>
+                    <div className='profile-list-search-name'>
+                        <input
+                            type='text'
+                            value={searchName}
+                            placeholder='Search by name'
+                            onChange={e => setSearchName(e.target.value)}
+                        />
+                    </div>
+                    <div className='profile-list-search-tag'>
+                        <input
+                            type='text'
+                            value={searchTag}
+                            placeholder='Search by tag'
+                            onChange={e => setSearchTag(e.target.value)}
+                        />
+                    </div>
+                    {profiles.map((item) => (
+                        <div key={item.id} className='profile-content'>
+                            <div className='profile-content-img'>
+                                <img
+                                    src={item.pic}
+                                    alt={item.pic}
+                                />
+                            </div>
+                            <div className='profile-content-info'>
+                                <div className='profile-content-title'>{item.frstName} {item.lastName}</div>
+                                <div className='profile-content-data'>
+                                    <span>Email: {item.email}</span><br />
+                                    <span>Company: {item.company}</span><br />
+                                    <span>Skill: {item.skill}</span><br />
+                                    <span>Avarage: 88.875%</span>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            )}
+        </>
     );
 }
 
